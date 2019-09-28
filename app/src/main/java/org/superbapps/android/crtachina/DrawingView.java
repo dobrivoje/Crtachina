e088dd15467b7dd96aace325f9a429a87298b975
AUGUSTUS\root <dobri7@gmail.com>
2019-09-28 08:58:59 +0200

    adding an Arrow drawing tool

--- app/src/main/java/org/superbapps/android/crtachina/DrawingView.java
+++ app/src/main/java/org/superbapps/android/crtachina/DrawingView.java
@@ -19,205 +19,216 @@ import static java.lang.Math.sin;
 import static java.lang.Math.sqrt;
 
 public class DrawingView extends View {
-	private static final String TAG = "DrawingView";
 
-	Context context;
+    private static final String TAG = "DrawingView";
 
-	private Bitmap mBitmap;
-	private Canvas mCanvas;
-	private Path   mPath;
-	private Paint  mBitmapPaint;
+    Context context;
 
-	private Paint mPaint;
-	private float X1, Y1;
-	private float X2, Y2;
-	private static final float TOUCH_TOLERANCE = 4;
+    private Bitmap mBitmap;
+    private Canvas mCanvas;
+    private Path mPath;
+    private Paint mBitmapPaint;
 
-	// pointer = a shape (little circle) which we draw with
-	private Paint pointerPaint;
-	private Path  pointerPath;
+    private Paint mPaint;
+    private float X1, Y1;
+    private float X2, Y2;
+    private static final float TOUCH_PIXELS_TOLERANCE = 4;
 
-	private PalleteTools drawingShape;
+    // pointer = a shape (little circle) which we draw with
+    private Paint pointerPaint;
+    private Path pointerPath;
 
-	//<editor-fold desc="initialization">
-	public DrawingView(Context ctx) {
-		super( ctx );
-		context = ctx;
+    private PalleteTools drawingShape;
 
-		drawingShape = PalleteTools.ARROW;
-		initGUI();
-	}
+    //<editor-fold desc="initialization">
+    public DrawingView(Context ctx) {
+        super(ctx);
+        context = ctx;
 
-	private void initGUI() {
-		mPath = new Path();
-		mBitmapPaint = new Paint( Paint.DITHER_FLAG );
+        drawingShape = PalleteTools.ARROW;
+        initGUI();
+    }
 
-		mPaint = new Paint();
-		mPaint.setAlpha( 255 );
-		mPaint.setAntiAlias( true );
-		mPaint.setDither( true );
-		mPaint.setColor( Color.RED );
-		mPaint.setStyle( Paint.Style.STROKE );
-		mPaint.setStrokeJoin( Paint.Join.ROUND );
-		mPaint.setStrokeCap( Paint.Cap.ROUND );
-		mPaint.setStrokeWidth( 12 );
+    private void initGUI() {
+        mPath = new Path();
+        mBitmapPaint = new Paint(Paint.DITHER_FLAG);
 
-		configureCirclePointer();
-	}
+        mPaint = new Paint();
+        mPaint.setAlpha(255);
+        mPaint.setAntiAlias(true);
+        mPaint.setDither(true);
+        mPaint.setColor(Color.RED);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeJoin(Paint.Join.ROUND);
+        mPaint.setStrokeCap(Paint.Cap.ROUND);
+        mPaint.setStrokeWidth(33);
 
-	private void configureCirclePointer() {
-		pointerPaint = new Paint();
-		pointerPath = new Path();
-		pointerPaint.setAntiAlias( true );
-		pointerPaint.setColor( Color.GRAY );
-		pointerPaint.setStyle( Paint.Style.STROKE );
-		pointerPaint.setStrokeJoin( Paint.Join.MITER );
-		pointerPaint.setStrokeWidth( 4f );
-	}
-	//</editor-fold>
+        configureCirclePointer();
+    }
 
-	//<editor-fold desc="system overrides">
-	@Override
-	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-		super.onSizeChanged( w, h, oldw, oldh );
+    private void configureCirclePointer() {
+        pointerPaint = new Paint();
+        pointerPath = new Path();
+        pointerPaint.setAntiAlias(true);
+        pointerPaint.setColor(Color.GRAY);
+        pointerPaint.setStyle(Paint.Style.STROKE);
+        pointerPaint.setStrokeJoin(Paint.Join.MITER);
+        pointerPaint.setStrokeWidth(4f);
+    }
+    //</editor-fold>
 
-		mBitmap = Bitmap.createBitmap( w, h, Bitmap.Config.ARGB_8888 );
-		mCanvas = new Canvas( mBitmap );
-	}
+    //<editor-fold desc="system overrides">
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
 
-	@Override
-	protected void onDraw(Canvas canvas) {
-		super.onDraw( canvas );
+        mBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        mCanvas = new Canvas(mBitmap);
+    }
 
-		canvas.drawPath( pointerPath, pointerPaint );
-		canvas.drawPath( mPath, mPaint );
-		canvas.drawBitmap( mBitmap, 0, 0, mBitmapPaint );
-	}
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
 
-	@Override
-	public boolean onTouchEvent(MotionEvent event) {
-		float x = event.getX();
-		float y = event.getY();
+        canvas.drawPath(pointerPath, pointerPaint);
+        canvas.drawPath(mPath, mPaint);
+        canvas.drawBitmap(mBitmap, 0, 0, mBitmapPaint);
+    }
 
-		switch (event.getAction()) {
-		case MotionEvent.ACTION_DOWN:
-			touchStart( x, y );
-			break;
-		case MotionEvent.ACTION_MOVE:
-			touchMove( x, y );
-			break;
-		case MotionEvent.ACTION_UP:
-			touchUp();
-			break;
-		}
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float x = event.getX();
+        float y = event.getY();
 
-		invalidate();
-		return true;
-	}
-	//</editor-fold>
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                touchStart(x, y);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                touchMove(x, y);
+                break;
+            case MotionEvent.ACTION_UP:
+                touchUp();
+                break;
+        }
 
-	//<editor-fold desc="move handler">
-	private void touchStart(float x, float y) {
-		mPath.reset();
-		mPath.moveTo( x, y );
-		X2 = X1 = x;
-		Y2 = Y1 = y;
-	}
+        invalidate();
+        return true;
+    }
+    //</editor-fold>
 
-	private void touchMove(float x, float y) {
-		float dx = Math.abs( x - X2 );
-		float dy = Math.abs( y - Y2 );
-		if (dx >= TOUCH_TOLERANCE || dy >= TOUCH_TOLERANCE) {
-			// actual drawing
-			drawingToolSelector( x, y );
-			// end of actual drawing
+    //<editor-fold desc="move handler">
+    private void touchStart(float x, float y) {
+        mPath.reset();
+        mPath.moveTo(x, y);
+        X2 = X1 = x;
+        Y2 = Y1 = y;
+    }
 
-			X2 = x;
-			Y2 = y;
-			pointerPath.reset();
-			pointerPath.addCircle( x, y, 15, Path.Direction.CCW );
-		}
-	}
+    private void touchMove(float x, float y) {
+        float dx = Math.abs(x - X2);
+        float dy = Math.abs(y - Y2);
+        if (dx >= TOUCH_PIXELS_TOLERANCE || dy >= TOUCH_PIXELS_TOLERANCE) {
+            mPaint.setAlpha(60);
 
-	private void touchUp() {
-		// actual drawing
-		drawingToolSelector( X2, Y2 );
-		// end of actual drawing
+            // actual drawing
+            drawingToolSelector(x, y);
+            // end of actual drawing
 
-		mPaint.setAlpha( 255 );
+            // remember last position
+            X2 = x;
+            Y2 = y;
+            // ...and make an illusion of circle pointer moving
+            pointerPath.reset();
+            pointerPath.addCircle(x, y, 15, Path.Direction.CCW);
+        }
+    }
 
-		pointerPath.reset();
-		mCanvas.drawPath( mPath, mPaint );
-		mPath.reset();
-	}
-	//</editor-fold>
+    private void touchUp() {
+        drawingToolSelector(X2, Y2);
 
-	private void drawingToolSelector(float x, float y) {
-		mPaint.setAlpha( 60 );
+        mPaint.setAlpha(255);
 
-		switch (drawingShape) {
-		case FREE_HAND:
-			mPath.quadTo( X2, Y2, x, y );
-			break;
-		case LINE:
-			mPath.reset();
-			mPath.moveTo( X1, Y1 );
-			mPath.lineTo( x, y );
-			break;
-		case CIRCLE:
-			mPath.reset();
-			mPath.moveTo( X1, Y1 );
-			mPath.addCircle( X1, Y1, radius(), Path.Direction.CW );
-			break;
-		case RECTANGLE:
-			mPath.reset();
-			mPath.addRect( X1, Y1, X2, Y2, Path.Direction.CW );
-			break;
-		case ARROW:
-			mPath.reset();
-			mPath.moveTo( X1, Y1 );
-			mPath.lineTo( X2, Y2 );
+        pointerPath.reset();
+        mCanvas.drawPath(mPath, mPaint);
+        mPath.reset();
+    }
+    //</editor-fold>
 
-			float L = radius();
-			float L3 = L < 50 ? L : 50;
+    private void drawingToolSelector(float x, float y) {
+        float dy = Y2 - Y1;
+        float dx = X2 - X1;
 
-			// userDefinedArrowAngle :
-			float alpha = (float) (PI / 6);
+        switch (drawingShape) {
+            case FREE_HAND:
+                mPath.quadTo(X2, Y2, x, y);
+                break;
+            case LINE:
+                mPath.reset();
+                mPath.moveTo(X1, Y1);
+                mPath.lineTo(x, y);
+                break;
+            case CIRCLE:
+                mPath.reset();
+                mPath.moveTo(X1, Y1);
+                mPath.addCircle(X1, Y1, radius(), Path.Direction.CW);
+                break;
+            case RECTANGLE:
+                mPath.reset();
+                mPath.addRect(X1, Y1, X2, Y2, Path.Direction.CW);
+                break;
+            case ARROW:
+                mPath.reset();
+                mPath.moveTo(X1, Y1);
+                mPath.lineTo(X2, Y2);
 
-			// dinamicaly changing agle :
-			double thetha = atan( (Y2 - Y1) / (X2 - X1) );
+                float L = radius();
 
-			// pol coord system :
-			double beta = atan( L3 / (L - L3 * cos( alpha )) );
-			double r = (L - L3 * cos( alpha )) / cos( beta );
+                // length og the arrow side lines :
+                float L3 = L < 90 ? L : 90;
 
-			float x3 = (float) (X1 + (r * cos( thetha - beta )));
-			float y3 = (float) (Y1 - (r * sin( PI + thetha - beta )));
+                // userDefinedArrowAngle :
+                float alpha = (float) (PI / 6);
+                // controls sharpness of the arrow
+                // the less, the sharper
+                float angleFactor = 0.5f;
 
-			float x4 = (float) (X1 + (r * cos( thetha + beta )));
-			float y4 = (float) (Y1 - (r * sin( PI + thetha + beta )));
+                // dinamicaly adapt an agle once it reaches PI radians:
+                double thetha = atan(dy / dx);
+                thetha += dx < 0 ? PI : 0;
 
-			mPath.lineTo( x3, y3 );
-			mPath.lineTo( X2, Y2 );
-			mPath.lineTo( x4, y4 );
-			break;
+                // pol coord system :
+                double polAngle = angleFactor * atan(L3 / (L - L3 * cos(alpha)));
+                double polRadius = (L - L3 * cos(alpha)) / cos(polAngle);
 
-		default:
-			mPath.reset();
-			mPath.moveTo( X1, Y1 );
+                float x3 = (float) (X1 + (polRadius * cos(thetha - polAngle)));
+                float y3 = (float) (Y1 + (polRadius * sin(thetha - polAngle)));
 
-			float r1 = radius();
-			double teta = atan( (Y2 - Y1) / (X2 - X1) );
+                float x4 = (float) (X1 + (polRadius * cos(thetha + polAngle)));
+                float y4 = (float) (Y1 + (polRadius * sin(thetha + polAngle)));
 
-			float xx = (float) (X1 + r1 * cos( teta ));
-			float yy = (float) (Y1 - r1 * sin( PI + teta ));
-			mPath.lineTo( xx, yy );
-		}
-	}
+                mPath.lineTo(x3, y3);
+                mPath.lineTo(X2, Y2);
+                mPath.lineTo(x4, y4);
+                break;
 
-	private float radius() {
-		return (float) sqrt( pow( X2 - X1, 2 ) + pow( Y2 - Y1, 2 ) );
-	}
+            default:
+                mPath.reset();
+                mPath.moveTo(X1, Y1);
+
+                float r1 = radius();
+                double teta = atan(dy / dx);
+                teta += dx < 0 ? PI : 0;
+
+                float xx = (float) (X1 + r1 * cos(teta));
+                float yy = (float) (Y1 + r1 * sin(teta));
+                mPath.lineTo(xx, yy);
+        }
+    }
+
+    private float radius() {
+        return (float) sqrt(pow(X2 - X1, 2) + pow(Y2 - Y1, 2));
+    }
 }
 
 
